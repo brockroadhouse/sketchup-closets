bcaa07ee8e39f3d3f26878b0e12f13528aadf398
Brock Roadhouse <brockroadhouse@gmail.com>
2019-03-19 15:58:35 -0700

    create walls dialog and floor drawers fix

--- su_closets/dialog.rb
+++ su_closets/dialog.rb
@@ -6,13 +6,28 @@ module Closets
     htmlFile = File.join(__dir__, 'html', 'dialog.html')
 
     options = {
-      :dialog_title => "Closet",
+      :dialog_title => "",
       :preferences_key => "com.fvcc.closets",
       :style => UI::HtmlDialog::STYLE_DIALOG
     }
     dialog = UI::HtmlDialog.new(options)
     dialog.set_file(htmlFile)
-    dialog.set_size(1000, 900)
+    dialog.set_size(600, 400)
+    dialog.center
+    dialog
+  end
+
+  def self.createRoomDialog
+    htmlFile = File.join(__dir__, 'html', 'room.html')
+
+    options = {
+      :dialog_title => "Create Walls",
+      :preferences_key => "com.fvcc.closets",
+      :style => UI::HtmlDialog::STYLE_DIALOG
+    }
+    dialog = UI::HtmlDialog.new(options)
+    dialog.set_file(htmlFile)
+    dialog.set_size(500, 400)
     dialog.center
     dialog
   end
@@ -23,8 +38,8 @@ module Closets
       self.update_dialog
       nil
     }
-    @dialog.add_action_callback("build") { |action_context, closet, left|
-      self.build(closet, left)
+    @dialog.add_action_callback("build") { |action_context, closet|
+      self.build(closet)
       @dialog.close
       nil
     }
@@ -35,16 +50,40 @@ module Closets
     @dialog.visible? ? @dialog.bring_to_front : @dialog.show
   end
 
-  def self.update_dialog
+  def self.showRoomDialog
+    @room_dialog ||= self.createRoomDialog
+    @room_dialog.add_action_callback("ready") { |action_context|
+      self.updateRoomDialog
+      nil
+    }
+    @room_dialog.add_action_callback("buildRoom") { |action_context, room|
+      self.buildRoom(room)
+      @room_dialog.close
+      nil
+    }
+    @room_dialog.add_action_callback("cancel") { |action_context, value|
+      @room_dialog.close
+      nil
+    }
+    @room_dialog.visible? ? @room_dialog.bring_to_front : @room_dialog.show
+  end
+
+  def self.updateRoomDialog
     closetHash = {
       :name => "Closet " + @@nameCount.to_s,
-      :width => 114,
+      :width => 50,
       :height => 84,
       :depthLeft => 24,
       :depthRight => 24,
       :wallHeight => 96,
-      :floor => 0
     }
+
+
+    closetJson  = JSON.pretty_generate(closetHash)
+    @room_dialog.execute_script("updateCloset(#{closetJson})")
+  end
+
+  def self.update_dialog
     types = [
       {:value => 'LH', :text => 'Long Hang'},
       {:value => 'DH', :text => 'Double Hang'},
@@ -55,28 +94,39 @@ module Closets
       {:value => 'eq', :text => 'Equal'},
       {:value => 'set', :text => '24'},
     ]
-    left = [
+    closets = [
       {
-        :type => '',
-        :size => '',
+        :type => '-',
+        :size => '-',
       }
     ]
 
 
-    closetJson  = JSON.pretty_generate(closetHash)
+    closetsJson  = JSON.pretty_generate(closets)
     typesJson   = JSON.pretty_generate(types)
     sizesJson   = JSON.pretty_generate(sizes)
-    leftJson    = JSON.pretty_generate(left)
-    @dialog.execute_script("updateCloset(#{closetJson}, #{typesJson}, #{sizesJson}, #{leftJson}, #{leftJson}, #{leftJson})")
+    @dialog.execute_script("updateCloset(#{closetsJson}, #{typesJson}, #{sizesJson})")
   end
 
-  def self.build(closet, left)
-    puts closet
-    puts left
-    return
+  def self.buildRoom(closet)
     startOperation('Build Walls', true, closet['name'])
 
-    buildWalls(closet['name'], closet['width'], closet['depthLeft'], closet['depthRight'], 6, 6, closet['height'], closet['wallHeight'])
+    buildWalls(
+      closet['name'],
+      closet['width'].to_l,
+      closet['depthLeft'].to_l,
+      closet['depthRight'].to_l,
+      closet['height'].to_l,
+      closet['wallHeight'].to_l
+    )
+
+    endOperation
+  end
+
+  def self.build(closet)
+    puts closet
+    return
+    startOperation('Build Closet')
 
     endOperation
   end
